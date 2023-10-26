aad363b7fb7bacb96ed9f3bb9fa6a773d794adec
Reza <rdalvand@yahoo.com>
2023-10-26 07:59:43 +0330

    added translation.py to site settings

--- site_settings/models.py
+++ site_settings/models.py
@@ -5,12 +5,12 @@ from django.utils.translation import gettext_lazy as _
 # Create your models here.
 class SiteSettings(models.Model):
     title = models.CharField(_('title'), max_length=90)
-    email = models.EmailField(_('email'))
+    email = models.EmailField(_('email'), null=True)
     short_description = models.TextField(_('short description'))
     description = models.TextField(_('description'))
-    address = models.TextField(_('address'))
-    mobile = models.IntegerField(_('phone number'))
-    phone_number = models.IntegerField(_('phone number'))
+    address = models.TextField(_('address'), null=True)
+    mobile = models.IntegerField(_('phone number'), null=True)
+    phone_number = models.IntegerField(_('phone number'), null=True)
 
     def __str__(self):
         return self.title
