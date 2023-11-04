36bf2e9b252ec7f71ff486e2855a5df17abd8ef1
Reza <rdalvand@yahoo.com>
2023-11-04 11:41:52 +0330

    complete comment api

--- products_module/serializers.py
+++ products_module/serializers.py
@@ -7,20 +7,11 @@ from .models import Product, ProductComment
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductComment
-        fields = ['product', 'full_name', 'email', 'message']
+        exclude = ['created_at']
 
 
 class ProductSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True)
-
-
-    def create(self, validated_data):
-        print(validated_data)
-        # tracks_data = validated_data.pop('tracks')
-        # album = Album.objects.create(**validated_data)
-        # for track_data in tracks_data:
-        #     Track.objects.create(album=album, **track_data)
-        return Response(status=201)
 
     class Meta:
         model = Product
--- products_module/urls.py
+++ products_module/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .api import ProductsListApi
+from .api import ProductApi
 from .views import ProductsListView, ProductsDetailView
 
 app_name = 'products'
 
 router = DefaultRouter()
-router.register('api/product-list', ProductsListApi, basename='products_api')
+router.register('api/product', ProductApi, basename='product_api')
 
 urlpatterns = [path('', ProductsListView.as_view(), name='products_list'),
                path('detail/<pk>/', ProductsDetailView.as_view(), name='product_detail')] + router.urls
