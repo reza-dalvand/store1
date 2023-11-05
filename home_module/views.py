b5c9663f4e6c64047abf461cc4237ebb5d1bdf57
Reza <rdalvand@yahoo.com>
2023-11-05 13:24:10 +0330

    show category in home page

--- home_module/views.py
+++ home_module/views.py
@@ -3,14 +3,21 @@ from django.utils import translation
 from django.views import View
 from rest_framework.permissions import AllowAny
 
+from products_module.models import ProductCategory
 from store import settings
 
 
 # Create your views here.
 
-def home(request):
-    return render(request, './index.html', {})
+class home(View):
 
+    def get(self, request):
+        categories = ProductCategory.objects.filter(parent__name=None)
+        context = {
+            'categories': categories,
+        }
+
+        return render(request, './index.html', context)
 
 def about_us(request):
     return render(request, './about-us/about-us.html', {})
